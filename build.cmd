8f28835ec9d9c44048fcac3d0aa14f1ade98d960
Rami Alia <ramialia@gmail.com>
2021-02-27 22:53:59 -0400

    build cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
-set JAVA_HOME=C:\Program Files\JDK
+rem set JAVA_HOME=C:\Program Files\JDK
+set JAVA_HOME=C:\Program Files\AdoptOpenJDK\jdk-8.0.242.08-hotspot
 c:\apache-maven\bin\mvn package -DskipTests=true
